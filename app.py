a195961b1db377a0c5a5dccf7a375b88232cdb4b
Thiago de Oliveira Dantas <t.dod@live.com>
2024-05-06 21:09:36 -0300

    up

--- app.py
+++ app.py
@@ -9,21 +9,43 @@ Digite a operação => """
 
 saldo = 0
 limite = 500
-extrato = ''
+extrato = []
+linha_extrato = 0
 numero_saques = 0
+ultima_operacao = ''
 LIMITES_SAQUES = 3
+
+
+def alimenta_extrato(operacao, valor):
+    global extrato
+    global linha_extrato
+
+    extrato.append(f"{linha_extrato}: {operacao}: R$ {valor}")
+
+
+def deposito(valor):
+    global saldo
+    global ultima_operacao
+    if valor <= 0:
+        print('Não é possivel depositar valores negativos ou zerados!')
+    else:
+        saldo += valor
+        ultima_operacao = 'Deposito'
+        alimenta_extrato(ultima_operacao, valor)
+
 
 while True:
     opcao = input(menu)
     if opcao == 'd':
         os.system('cls')
-        print('Deposito')
+        deposito(float(input()))
     elif opcao == 's':
         os.system('cls')
         print('Saque')
     elif opcao == 'e':
         os.system('cls')
-        print('Extrato')
+        for e in len(extrato):
+            print(extrato[e])
     elif opcao == 'q':
         break
     else:
