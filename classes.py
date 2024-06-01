97e6bcc14387064f8fe5cf2c5e58fef8ba22241c
Thiago de Oliveira Dantas <t.dod@live.com>
2024-05-31 23:18:16 -0300

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,19 @@
+class Conta:
+    def __init__(self, saldo, numero, agencia, cliente, historico):
+        self._saldo = saldo
+        self._numero = numero
+        self._agencia = agencia
+        self._cliente = cliente
+        self._historico = historico
+
+    def saldo(self):
+        pass
+
+    def nova_conta(self):
+        pass
+
+    def sacar(self):
+        pass
+
+    def depositar(self):
+        pass
