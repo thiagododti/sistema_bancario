e93a78450d5e97432ece454eaf020001373ba59f
Thiago de Oliveira Dantas <t.dod@live.com>
2024-06-02 23:34:51 -0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,19 +1,38 @@
 class Conta:
-    def __init__(self, saldo, numero, agencia, cliente, historico):
-        self._saldo = saldo
+    def __init__(self, numero, agencia, cliente, historico):
+        self._saldo = 0
         self._numero = numero
         self._agencia = agencia
         self._cliente = cliente
         self._historico = historico
 
     def saldo(self):
-        pass
+        return self._saldo
 
     def nova_conta(self):
         pass
 
-    def sacar(self):
-        pass
+    def sacar(self, valor):
+        if valor > self._saldo:
+            print(f"Saldo insuficiente! Saldo atual: R$ {self._saldo}")
+        else:
+            self._saldo -= valor
 
-    def depositar(self):
+    def depositar(self, valor):
+        self._saldo += valor
+
+
+class ContaCorrente(Conta):
+    def __init__(self, numero, agencia, cliente, historico,
+                 limite, limite_saques):
+        super().__init__(numero, agencia, cliente, historico)
+        self._limite = limite
+        self._limite_saques = limite_saques
+
+
+class Historico(Conta):
+    def __init__(self, numero, agencia, cliente, historico):
+        super().__init__(numero, agencia, cliente, historico)
+
+    def adicionar_transacao(self, transacao):
         pass
